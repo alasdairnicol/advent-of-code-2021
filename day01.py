aaaac8d2a125a55e0864fd010d4918e14c5aaf4e
Alasdair Nicol <alasdair@thenicols.net>
2021-12-01 18:51:32 +0000

    Remove empty line

--- day01.py
+++ day01.py
@@ -26,7 +26,6 @@ def num_increasing_values(depths, window_length=1):
 
     Comparing A = [199, 200, 208] with B = [200, 208, 210], both lists
     contain 200 and 208, so we only need to compare 199 and 210.
-
     """
     return sum(1 if b > a else 0 for a, b in zip(depths, depths[window_length:]))
 

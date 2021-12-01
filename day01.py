41bc122102474d401837215d52a852e8fb2ef147
Alasdair Nicol <alasdair@thenicols.net>
2021-12-01 18:41:02 +0000

    Revert day1/day2 changes

--- day01.py
+++ day01.py
@@ -6,7 +6,6 @@ def main():
     depths = list(read_input())
     part_a = num_increasing_values(depths)
     part_b = num_increasing_values(depths, window_length=3)
-
     print(f"{part_a=}")
     print(f"{part_b=}")
 
@@ -27,6 +26,7 @@ def num_increasing_values(depths, window_length=1):
 
     Comparing A = [199, 200, 208] with B = [200, 208, 210], both lists
     contain 200 and 208, so we only need to compare 199 and 210.
+
     """
     return sum(1 if b > a else 0 for a, b in zip(depths, depths[window_length:]))
 

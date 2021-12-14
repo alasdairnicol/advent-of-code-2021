6eb708e33a3937d059ad547a4e0171bf36e47539
Alasdair Nicol <alasdair@thenicols.net>
2021-12-14 17:17:30 +0000

    Day 14 - non recursive approach for part 2

--- day14.py
+++ day14.py
@@ -13,6 +13,8 @@ def main():
     print(f"{part_1=}")
 
     part_2 = do_part_2(template, rules)
+    part_2_not_recursive = do_part_2_not_recursive(template, rules)
+    assert part_2 == part_2_not_recursive
     print(f"{part_2=}")
 
 
@@ -56,6 +58,29 @@ def do_part_2(polymer, rules) -> int:
 
     for x, y in zip(polymer, polymer[1:]):
         totals += count(x, y, num_steps)
+
+    counts = totals.most_common()
+    most = counts[0][1]
+    least = counts[-1][1]
+    return most - least
+
+
+def do_part_2_not_recursive(polymer, rules) -> int:
+    num_steps = 40
+
+    pairs = Counter(zip(polymer, polymer[1:]))
+
+    for i in range(num_steps):
+        new_pairs: Counter = Counter()
+        for (x, y), num in pairs.items():
+            z = rules[x + y]
+            new_pairs[(x, z)] += num
+            new_pairs[(z, y)] += num
+        pairs = new_pairs
+
+    totals: Counter = Counter(polymer[-1:])
+    for (x, y), num in pairs.items():
+        totals[x] += num
 
     counts = totals.most_common()
     most = counts[0][1]

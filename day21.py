71cd8666d94a658cc100ba25828a9d7a34204e97
Alasdair Nicol <alasdair@thenicols.net>
2021-12-21 12:07:36 +0000

    Day 21 part 2

--- day21.py
+++ day21.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
-from os import read
-from itertools import cycle
-from typing import Mapping, Iterator
+from collections import Counter
+from dataclasses import dataclass
+from itertools import cycle, product
+from typing import Mapping, Iterator, Tuple
+
+
+@dataclass
+class Universe:
+    positions: Tuple[int, int]
+    scores: Tuple[int, int]
+    player_1_next: bool
+
+    def __hash__(self):
+        return hash((self.positions, self.scores, self.player_1_next))
 
 
 def roll(dice: Iterator[int]) -> int:
     return sum([next(dice), next(dice), next(dice)])
+
+
+def calc_distribution(values: list[int], num_rolls: int) -> Mapping[int, int]:
+    distribution: Mapping[int, int] = Counter()
+    return Counter(sum(dice) for dice in product(values, repeat=num_rolls))
 
 
 def main():
@@ -16,8 +32,8 @@ def main():
     part_1 = do_part_1(starting_pos_1, starting_pos_2)
     print(f"{part_1=}")
 
-    # part_2 = 10
-    # print(f"{part_2=}")
+    part_2 = do_part_2(starting_pos_1, starting_pos_2)
+    print(f"{part_2=}")
 
 
 def do_part_1(starting_pos_1: int, starting_pos_2: int) -> int:
@@ -45,6 +61,49 @@ def do_part_1(starting_pos_1: int, starting_pos_2: int) -> int:
     losing_score = scores[1]
 
     return roll_count * losing_score
+
+
+def do_part_2(starting_pos_1: int, starting_pos_2: int) -> int:
+    target_score = 21
+    distribution = calc_distribution([1, 2, 3], 3)
+
+    winning_universes = [0, 0]
+
+    universe = Universe(
+        (starting_pos_1, starting_pos_2),
+        (0, 0),
+        True,
+    )
+
+    universes = Counter([universe])
+
+    while universes:
+        # FIXME try choosing a better universe to process e.g. lowest score
+        universe = list(universes.keys())[0]
+        count = universes.pop(universe)
+
+        for roll, distribution_count in distribution.items():
+            new_pos = universe.positions[0] + roll
+            if new_pos > 10:
+                new_pos = new_pos % 10
+            new_score = universe.scores[0] + new_pos
+            new_count = count * distribution_count
+
+            if new_score >= target_score:
+                if universe.player_1_next:
+                    winning_universes[0] += new_count
+                else:
+                    winning_universes[1] += new_count
+
+            else:
+                new_universe = Universe(
+                    (universe.positions[1], new_pos),
+                    (universe.scores[1], new_score),
+                    not universe.player_1_next,
+                )
+                universes.update({new_universe: new_count})
+
+    return max(winning_universes)
 
 
 def read_input() -> list[str]:
